40a60519bc2c00a1132dc25d43a8387c52a5e769
Muhammad Ramzan <ramzan11596@gmail.com>
2018-12-07 11:45:45 -0800

    Refs

---
+++ refs/app.js
@@ -0,0 +1,13 @@
+new Vue({
+  el: '#vue-app',
+  data: {
+    output: 'your fav food'
+  },
+  methods: {
+    readRefs: function()
+    {
+      console.log(this.$refs.test.innerText);
+      this.output = this.$refs.input.value;
+    }
+  }
+});
