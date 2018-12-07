8d97b4471e2d90d044ec715d56d702971e7db41c
Muhammad Ramzan <ramzan11596@gmail.com>
2018-12-07 11:07:59 -0800

    Multiple Vue Instance

---
+++ multiple-vue-instance/app.js
@@ -0,0 +1,34 @@
+var one =new Vue({
+  el: '#vue-app-one',
+  data: {
+    title: 'Vue App One'
+  },
+  methods: {
+    
+  },
+  computed: {
+    greet: function(){
+      return 'Hello from app one :)';
+    }
+  }
+});
+
+
+var two =new Vue({
+  el: '#vue-app-two',
+  data: {
+    title: 'Vue App Two'
+  },
+  methods: {
+    changeTitle: function(){
+      one.title = "Title Changed"
+    }
+  },
+  computed: {
+    greet: function(){
+      return 'Yo dude! This is app 2 speaking you';
+    }
+  }
+});
+
+two.title = "Changed from outside"
